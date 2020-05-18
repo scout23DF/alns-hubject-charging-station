32135c3ae1595c9edb80b7b8d8e69d1caaa8abf5
Andre Luiz do Nascimento Sousa <scout23df@gmail.com>
2020-05-18 21:37:42 +0200

    Initializing the project.

---
+++ src/main/java/de/com/alns/codingtest/hubject/chargingstationdata/HubjectChargingStationDataApplication.java
@@ -0,0 +1,13 @@
+package de.com.alns.codingtest.hubject.chargingstationdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubjectChargingStationDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubjectChargingStationDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/com/alns/codingtest/hubject/chargingstationdata/HubjectChargingStationDataApplicationTests.java
@@ -0,0 +1,13 @@
+package de.com.alns.codingtest.hubject.chargingstationdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubjectChargingStationDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
